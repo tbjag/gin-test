944a3aef1c1e673107fb07cbc63d9c7728a649c9
tbjag <tbjagodits@gmail.com>
2023-10-05 10:06:47 -0700

    added files

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"example/web-service-gin/controller"
+	"example/web-service-gin/middleware"
+	"example/web-service-gin/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middleware.Logger())
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
