543a531fbe0e77bcbedbea98f5aeebf204a842ab
tbjagodits <tbjagodits@gmail.com>
2023-10-20 18:27:18 -0700

    auth

--- server.go
+++ server.go
@@ -16,7 +16,7 @@ var (
 func main() {
 	server := gin.New()
 
-	server.Use(gin.Recovery(), middleware.Logger())
+	server.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
